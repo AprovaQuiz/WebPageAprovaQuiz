8b2212700bfa382b5fbb7b59b7f840ab2d29038b
Lucas Albuquerque <75431427+spartan117navy@users.noreply.github.com>
2024-10-02 15:44:06 -0300

    alterada basica para testar

---
+++ app/components/Footer.tsx
@@ -0,0 +1,104 @@
+import { Link } from "@remix-run/react";
+
+export function Footer() {
+
+    return (
+        <footer className="container-fluid">
+            
+            <div className="row">
+                <div className="col d-flex justify-content-center align-items-center pb-4 pt-4">
+                    <ul className="footer-list">
+                        <li>
+                            <h3 className="footerTitulo">Cadernos</h3>
+                        </li>
+                        <li>
+                            <Link to="#" className="footer-link">Ciências Humanas</Link>
+                        </li>
+                        <li>
+                            <Link to="#" className="footer-link">Ciências da Natureza</Link>
+                        </li>
+                        <li>
+                            <Link to="#" className="footer-link">Matemática</Link>
+                        </li>
+                        <li>
+                            <Link to="#" className="footer-link">Linguagens</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col pb-4 pt-4">
+                    <ul className="footer-list">
+                        <li>
+                            <h3 className="footerTitulo">Notícias</h3>
+                        </li>
+                        <li>
+                            <Link to="#" className="footer-link">ENEM</Link>
+                        </li>
+                        <li>
+                            <Link to="#" className="footer-link">SISU</Link>
+                        </li>
+                        <li>
+                            <Link to="#" className="footer-link">ProUni</Link>
+                        </li>
+                        <li>
+                            <Link to="#" className="footer-link">FUVEST</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col pb-4 pt-4">
+                    <ul className="footer-list">
+                        <li>
+                            <h3 className="footerTitulo">Perfil</h3>
+                        </li>
+                        <li>
+                            <Link to="#" className="footer-link">Configurações</Link>
+                        </li>
+                        <li>
+                            <Link to="#" className="footer-link">Simulados</Link>
+                        </li>
+                        <li>
+                            <Link to="#" className="footer-link">Histórico</Link>
+                        </li>
+                        <li>
+                            <Link to="#" className="footer-link">Notificações</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col pb-4 pt-4">
+                    <ul className="footer-list">
+                        <li>
+                            <h3 className="footerTitulo">Download</h3>
+                        </li>
+                        <li>
+                            <Link to="#" className="footer-link">IOS</Link>
+                        </li>
+                        <li>
+                            <Link to="#" className="footer-link">Android</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col pb-4 pt-4">
+                    <ul className="footer-list">
+                        <li>
+                            <h3 className="footerTitulo">Redes Sociais</h3>
+                        </li>
+                        <li>
+                            <Link to="#" className="footer-link">Facebook</Link>
+                        </li>
+                        <li>
+                            <Link to="#" className="footer-link">Twitter</Link>
+                        </li>
+                        <li>
+                            <Link to="#" className="footer-link">Instagram</Link>
+                        </li>
+                        <li>
+                            <Link to="#" className="footer-link">LinkedIn</Link>
+                        </li>
+                        <li>
+                            <Link to="#" className="footer-link">YouTube</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    )
+}
---
+++ app/components/Header.tsx
@@ -0,0 +1,17 @@
+
+export function Header() {
+
+    return (
+        <header>
+            <nav className="headerAQ navbar ">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">
+                    <img src="/LogoAprovaQuiz.png" alt="Logo" width="50" height="60" className="d-inline-block align-text-top"></img>
+                    
+                    </a>
+                </div>
+            </nav>
+        </header >
+    )
+
+}
