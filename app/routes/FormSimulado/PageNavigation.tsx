482001b0c8fd599dc3c3c28d6acf0139f98a0143
Lucas Albuquerque <75431427+spartan117navy@users.noreply.github.com>
2024-11-05 17:30:19 -0300

    assuntos

--- app/routes/FormSimulado/PageNavigation.tsx
+++ app/routes/FormSimulado/PageNavigation.tsx
@@ -3,6 +3,7 @@ import { dataCadernos } from "../_index/carousel";
 import { Grid } from "./GridSimpleCard";
 import { GridMateria, ImageInterface } from "./GridMateria";
 import { axiosAprovaApi } from "~/configs/auth";
+/*import { GridAssunto, ImageInterface } from "./GridAssunto";*/
 
 export default function PageNavigation() {
 
@@ -10,12 +11,18 @@ export default function PageNavigation() {
 
     const [caderno, setCaderno] = useState("")
     const [materias, setMaterias] = useState<{ nome: string; image: ImageInterface; pertence: string }[]>([])
+    /*const [assuntos, setAssunto] = useState<{ nome: string; image: ImageInterface; materia: string }[]>([])*/
     const [materiaEscolhida, setMateriaEscolhida] = useState("")
+    /*const [assuntoEscolhido, setAssuntoEscolhido] = useState("")*/
 
     async function HandleAxios() {
         await axiosAprovaApi.get("/subjects/withImages")
             .then((r) => { setMaterias(r.data) })
             .catch(() => { throw new Error("Falha ao carregar Matérias") })
+
+        /*await axiosAprovaApi.get("/topics/withImages")
+        .then((r) => { setAssunto(r.data) })
+        .catch(() => { throw new Error("Falha ao carregar Assuntos") })*/
     }
 
     if (numeroPagina == 0) {
@@ -42,7 +49,24 @@ export default function PageNavigation() {
             </>
         )
     }
+/*
+    else if (numeroPagina == 2) {
 
+        HandleAxios()
+
+        const filteredAssuntos = materiaEscolhida != "Todos os Assuntos" ?
+            assuntos.filter((assunto) => assunto.materia == materiaEscolhida) :
+            assuntos
+
+        return (
+            <>
+                <p className="txtSimulado">Sobre qual assunto você quer fazer o Simulado?</p>
+                <GridAssunto assuntos={filteredAssuntos} setNumeroPagina={setNumeroPagina} setTipoDado={setAssuntoEscolhido} numeroPagina={numeroPagina} />
+                <button type="button" className="btn float-left m-5" onClick={() => setNumeroPagina(numeroPagina - 1)}>Voltar</button >
+            </>
+        )
+    }
+*/
     else {
         return (
             <>
